f09e25df103ee5f74ca672001b3c2785c2c827e2
Himanshu Gusain <himu5493@gmail.com>
2023-08-09 17:23:43 +0000

    todo exercise

---
+++ os/networks/examlple-execise.c
@@ -0,0 +1,19 @@
+
+
+
+// Service A connect to Service B
+
+
+// Service A
+// Get service B address
+// Connect
+// Send "ping"
+// Recieve "pong"
+// Close ( shutdown )
+
+
+// Service B
+// Listen on IP & Port
+// Recieve "ping"
+// Send "pong"
+// Close
